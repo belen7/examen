0beeb2e46449c8ca1dbf5c4fc5163f4c4179338d
Javier Fontanellaz <jfontanellaz@gmail.com>
2022-09-28 18:22:20 -0300

    se le agrego el config.dist.php

---
+++ conexion/config.dist.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ *  Archivo: config.php
+ *	Utilidad: Este archivo define constantes necesarias para la conexion con la base de datos.
+*/
+
+///////////////////// BASE DE DATOS local ///////////////////////////////////////////
+define('DB_TYPE', "mysql");
+define('DB_HOST', "localhost");
+define('DB_PORT', "3306");
+define('DB_NAME', "");
+define('DB_USER', "");
+define('DB_PASS', "");
+//////////////////////////////////////////////////////////////////////////////////////
+///////////////////// DEFINIMOS UN SECRETO ///////////////////////////////////////////
+define('SECRETO', "mi_secreto");
+?>
