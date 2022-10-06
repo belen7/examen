a921665e42d16744820b27b771ee12a25ea0315b
Javier Hernan Fontanellaz <jfontanellaz@santafe.gov.ar>
2022-10-06 12:22:00 -0300

    Se Agrego los Usuarios del sistema para poder ingresar al sistema

--- sistema/funciones/usuarioAutenticar.php
+++ sistema/funciones/usuarioAutenticar.php
@@ -9,7 +9,7 @@ include_once 'Sanitize.class.php';
 /**************************************************************** RECIBIR PARAMETROS Y SANITIZARLOS *******************************************************************/
 /**********************************************************************************************************************************************************************/
 
-$email = isset($_POST['usuario'])?SanitizeVars::STRING($_POST['usuario'],2,50):false;
+$usuario = isset($_POST['usuario'])?SanitizeVars::STRING($_POST['usuario'],2,50):false;
 $password = isset($_POST['password'])?SanitizeVars::STRING_LETRAS_NUMEROS($_POST['password'],8,15):false;
 
 //var_dump($_POST);
@@ -22,30 +22,47 @@ $entidad = "Interesado";
 $array_resultados = array();
 
 
-if ($email && $password) {
-      $msg = "";
-      $passwordMD5 = md5($password);
+$SYS_USERS = json_decode(SYS_USERS,true);
+//var_dump(array_keys($SYS_USERS));die;
 
-      // SE INICIA LA TRANSACCION 
-      $sql = "SELECT u.*, ur.tipo 
-              FROM usuario u, usuario_rol ur
-              WHERE u.email='$email' and u.password='$passwordMD5' and u.rol_id = ur.id";
-      $res = @mysqli_query($conex,$sql);
+if ($usuario && $password) {
 
-      //die($sql.'**'.mysqli_num_rows($res));
-
-      if (mysqli_num_rows($res)==1) {
-            $array_resultados['codigo'] = 100;
-            $array_resultados['mensaje'] = 'Entro'; 
-            $fila = mysqli_fetch_assoc($res);
-            $rol = $fila['tipo'];
-            $_SESSION['user_usuario'] = $fila['usuario'];
-            $_SESSION['user_email'] = $fila['email'];
-            $_SESSION['user_rol'] = unserialize($rol)[0];
+      if (in_array($usuario,array_keys($SYS_USERS))) {
+            //die("USUARIO: ".$usuario);
+            foreach($SYS_USERS as $indice => $valor) {
+                  if ($indice==$usuario && $valor==$password) {
+                        $_SESSION['user_usuario'] = $usuario;
+                        $_SESSION['user_email'] = "";
+                        $_SESSION['user_rol'] = 'SYSTEM';
+                        $_SESSION['user_providers'] = 'PROVIDER_SYSTEM';
+                        $array_resultados['codigo'] = 100;
+                        $array_resultados['mensaje'] = 'Entro'; 
+                  };
+            }
       } else {
-            $array_resultados['codigo'] = 10;
-            $array_resultados['mensaje'] = 'No coincide.'; 
-      }
+            $msg = "";
+            $passwordMD5 = md5($password);
+
+            // SE INICIA LA TRANSACCION 
+            $sql = "SELECT u.*, ur.tipo 
+                  FROM usuario u, usuario_rol ur
+                  WHERE u.email='$usuario' and u.password='$passwordMD5' and u.rol_id = ur.id";
+            $res = @mysqli_query($conex,$sql);
+
+            if (mysqli_num_rows($res)==1) {
+                  $array_resultados['codigo'] = 100;
+                  $array_resultados['mensaje'] = 'Entro'; 
+                  $fila = mysqli_fetch_assoc($res);
+                  $rol = $fila['tipo'];
+                  $_SESSION['user_usuario'] = $fila['usuario'];
+                  $_SESSION['user_email'] = $fila['email'];
+                  $_SESSION['user_rol'] = unserialize($rol)[0];
+                  $_SESSION['user_providers'] = 'PROVIDER_DB';
+            } else {
+                  $array_resultados['codigo'] = 10;
+                  $array_resultados['mensaje'] = 'No coincide.'; 
+            }
+      };
 } else {
       $array_resultados['codigo'] = 11;
       $array_resultados['mensaje'] = 'Faltan datos obligatorios.'; 
